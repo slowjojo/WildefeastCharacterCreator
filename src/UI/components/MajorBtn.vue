5b4313062f791d170a363b7ccecf54c37cc2dc31
Joern Gutzmer <joernarne@gmail.com>
2025-04-23 15:52:54 +0200

    edit to main menu

---
+++ src/UI/components/MajorBtn.vue
@@ -0,0 +1,11 @@
+<template>
+<button>
+    hello world
+</button>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
--- src/features/main_menu/desktop.vue
+++ src/features/main_menu/desktop.vue
@@ -1,6 +1,8 @@
 <template>
  <!-- top nav -->
-
+<div>
+  <MajorBtn></MajorBtn>
+</div>
   <!-- compendium button -->
   <!-- catalogue button -->
   <!-- compendium button -->
@@ -12,6 +14,8 @@
 </template>
 
 <script setup lang="ts">
+import MajorBtn from '../../UI/components/MajorBtn.vue';
+
 // nothing needed for now
 </script>
 
